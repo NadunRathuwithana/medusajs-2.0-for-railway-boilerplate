03611f9a79fe8c23583b4fe98b2d0e83517f9319
nadunrathuwithana98 <nadunios98@gmail.com>
2026-07-09 23:40:26 +0530

    Fix email module exporting both providers causing channel override

--- backend/src/modules/email-notifications/index.ts
+++ backend/src/modules/email-notifications/index.ts
@@ -2,7 +2,13 @@ import { ModuleProviderExports } from '@medusajs/framework/types'
 import { ResendNotificationService } from './services/resend'
 import { SmtpNotificationService } from './services/smtp'
 
-const services = [ResendNotificationService, SmtpNotificationService]
+const services = []
+
+if (process.env.RESEND_API_KEY) {
+  services.push(ResendNotificationService)
+} else {
+  services.push(SmtpNotificationService)
+}
 
 const providerExport: ModuleProviderExports = {
   services,
